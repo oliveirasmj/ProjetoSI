aa233e56ff4a7af42c4c295d4a2c722e386a2282
Hugo Pinho <hugopinho0@gmail.com>
2020-12-19 18:10:59 +0000

    Adicionado Jframe 2x

--- Programa/src/Home/Program.java
+++ Programa/src/Home/Program.java
@@ -1,9 +1,22 @@
+package Home;
 
-public class Program {
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Program extends JFrame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Worldddddd!");
+		Program home = new Program();
+		home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		home.setSize(500, 500);
+		home.setVisible(true);
+		home.setTitle("Home");
+		home.setLocationRelativeTo(null);
+		
+		JLabel lblText = new JLabel("Hello World");
+		lblText.setBounds(125,125,300,125);
+		home.add(lblText);
 	}
 
 }
