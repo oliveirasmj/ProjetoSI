df5602a2b51a017b537242827c258623077b69f8
Hugo Pinho <hugopinho0@gmail.com>
2021-01-02 11:09:49 +0000

    Atualizar repo

--- Programa/src/Home/Program.java
+++ Programa/src/Home/Program.java
@@ -1,64 +1,54 @@
 package Home;
 
-
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.text.ParseException;
 import java.util.Scanner;
 
 import javax.sound.midi.Soundbank;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import org.json.JSONException;
+
 import com.main.BibControlo;
 
-
-
+import UserInf.UserInfo;
 
 public class Program extends JFrame {
 
-	public static void main(String[] args) {
-			
-		BibControlo licenca = new BibControlo();
-		
-		boolean ficheiroComLicenca = false;
-		//procurar ficheiro de licenca
-		
+	public static void main(String[] args) throws CertificateException, IOException, ParseException, JSONException {
 
-		if(ficheiroComLicenca) {
-			//tem ficheiro com licenca
-			//depois e validar
+		BibControlo cb = new BibControlo();
+
+		if (cb.verificaSeLicencaExiste()) {
+			// tem ficheiro com licenca
+			// depois e validar
 			Program home = new Program();
 			home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			home.setSize(500, 500);
 			home.setVisible(true);
 			home.setTitle("Home");
 			home.setLocationRelativeTo(null);
-			
+
 			JLabel lblText = new JLabel(System.getProperty("os.name"));
-			lblText.setBounds(125,125,300,125);
+			lblText.setBounds(125, 125, 300, 125);
 			home.add(lblText);
-		}else {
-			//nao encontra a licenca
-			//questionar se quer criar uma nova licenca
-			
+		} else {
 			Scanner sc = new java.util.Scanner(System.in);
 			System.out.println("Não foi encontrada nenhuma licenaça válida deseja criar uma? ");
 			System.out.println("Se sim prima 1. Caso deseje terminar a execução prima 0");
 			int opcao = sc.nextInt();
-			switch(opcao) {
-			  case 1:
-				  licenca.criaLicenca();
-			    break;
-			  case 0:
-				  System.exit(0);
-			    break;
-			 
-			}
-			
-		}
-		
-		
-		
-		
-		
-	}
+			switch (opcao) {
+			case 1:
+				cb.registarNovaLicenca();
+				System.out.println("Licenca criada, necessário autor validar a sua licenca");
+				break;
+			case 0:
+				System.exit(0);
+				break;
 
+			}
+		}
+	}
 }
